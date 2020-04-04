2fbe74da411b15df790fa4ec243438c1d66986f4
Sergey-Pravdyukov <sergey.pravdyukov@gmail.com>
2020-04-04 16:33:27 +0300

    Refactored convert_annotated_RTSD_data_to_YOLO_format.py

--- dataset tools/convert_annotated_RTSD_data_to_YOLO_format.py
+++ dataset tools/convert_annotated_RTSD_data_to_YOLO_format.py
@@ -4,13 +4,14 @@ import cv2
 import numpy as np
 import click
 
-def find_labels(img_list, annotation_lines, dataset_part, labels_dict, dataset_path):
+def find_labels(img_list, dataset_part, dataset_path, dataset_annotations_path):
+	annotation_lines = get_annotation_lines(dataset_annotations_path)
+	labels_dict = get_all_labels(annotation_lines)
 	img_annotations = []
 	for img_name in img_list:
 		found = False
 		for line in annotation_lines:
 			if line[0] == img_name:
-				# img_annotations_line = ' '.join(line)
 				found = True
 				img_annotations.append(line)
 		if not found:
@@ -20,13 +21,25 @@ def find_labels(img_list, annotation_lines, dataset_part, labels_dict, dataset_p
 	for annotation in img_annotations:
 		img_name = annotation[0]
 		img = cv2.imread(os.path.join(dataset_path, 'images', dataset_part, img_name))
-		# print("img shape:", img.shape)
-		cur_annotation = convert_label(annotation[-1], labels_dict) + ' ' + convert_points_to_YOLO_format(' '.join(annotation[1:-1]), img.shape[:-1])
-		# print(os.path.join(dataset_path, 'labels', dataset_part, img_name[:-3] + 'txt'))
+		converted_annotation = convert_label(annotation[-1], labels_dict) + ' ' + convert_points_to_YOLO_format(' '.join(annotation[1:-1]), img.shape[:-1])
 		file = open(os.path.join(dataset_path, 'labels', dataset_part, img_name[:-3] + 'txt'), 'a')
-		file.write(cur_annotation + '\n')
+		file.write(converted_annotation + '\n')
 		file.close()
-	
+
+def get_annotation_lines(dataset_annotations_path):
+	annotation_lines = []
+	with open(dataset_annotations_path) as annotations:
+		annotations_reader = csv.reader(annotations)
+
+		next(annotations_reader)
+		next(annotations_reader)
+		next(annotations_reader)
+		next(annotations_reader)
+
+		for line in annotations_reader:
+			annotation_lines.append(line)
+	return annotation_lines
+
 def get_all_labels(annotation_lines):
 	labels = {}
 	counter = 0
@@ -45,15 +58,11 @@ def convert_points_to_YOLO_format(annotation, img_size):
 	x, y, w, h = int(x), int(y), int(w), int(h)
 
 	w, h = x + w, y + h
-	# print("most points:", x, y, w, h)
 	
 	center_x = int((x + w) / 2)
 	center_y = int((y + h) / 2) 
 	w = abs(x - w)
 	h = abs(y - h)
-
-	# print("img size:", total_w, total_h)
-	# print("like ints:", center_x, center_y, w, h)
 
 	center_x = float(center_x) / total_w
 	center_y = float(center_y) / total_h
@@ -61,7 +70,6 @@ def convert_points_to_YOLO_format(annotation, img_size):
 	h = float(h) / total_h
 
 	center_x, center_y, w, h = str(center_x), str(center_y), str(w), str(h)
-	# print("floats:", center_x, center_y, w, h, '\n')
 	return(center_x + ' ' + center_y + ' ' + w + ' ' + h)
 
 @click.command()
@@ -69,30 +77,12 @@ def convert_points_to_YOLO_format(annotation, img_size):
 @click.option('--dataset_annotations_path', default='../pruned_RTSD/detection/rtsd-d1-gt/rtsd-d1-gt_full.csv', help='Relative path to full datase toannotation files.')
 def convert_annotated_RTSD_data_to_YOLO_format(dataset_path, dataset_annotations_path):
 	dataset_parts = ['train', 'val', 'test']
-	labels_dict = {}
-	annotation_lines = []
-
-	with open(dataset_annotations_path) as annotations:
-		annotations_reader = csv.reader(annotations)
-
-		next(annotations_reader)
-		next(annotations_reader)
-		next(annotations_reader)
-		next(annotations_reader)
-
-		for line in annotations_reader:
-			annotation_lines.append(line)
-		labels_dict = get_all_labels(annotation_lines)
-
 	for dataset_part in dataset_parts:
 		file = open(os.path.join(dataset_path, 'images', dataset_part, (dataset_part + '_images.txt')), 'r')
 		img_list = file.read()
 		img_list = img_list.split('\n')	
-		find_labels(img_list, annotation_lines, dataset_part, labels_dict, dataset_path) 			
-	# # for k, v in labels_dict.items():
-	# # 	print(k)
+		find_labels(img_list, dataset_part, dataset_path, dataset_annotations_path) 			
 
 if __name__ == '__main__':
 	convert_annotated_RTSD_data_to_YOLO_format()
-	# convert_annotated_RTSD_data_to_YOLO_format()
 	
