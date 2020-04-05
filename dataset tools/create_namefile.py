a3e06262ab02a3b706de5ee94c3f27e3bc96f0c4
Sergey-Pravdyukov <sergey.pravdyukov@gmail.com>
2020-04-05 14:02:03 +0300

    Added create namefiles option. Updated makefile with new target.

---
+++ dataset tools/create_namefile.py
@@ -0,0 +1,16 @@
+import os
+import click
+from convert_annotated_RTSD_data_to_YOLO_format import get_annotation_lines, get_all_labels
+
+@click.command()
+@click.option('--dataset_annotations_path', default='../pruned_RTSD/detection/rtsd-d1-gt/rtsd-d1-gt_full.csv', help='Relative path to full datase toannotation files.')
+@click.option('--target_dir', default='../config', help='Path to created *.name')
+def create_namefile(dataset_annotations_path, target_dir):
+	annotation_lines = get_annotation_lines(dataset_annotations_path)
+	labels_dict = get_all_labels(annotation_lines)
+	file = open(os.path.join(target_dir, 'rtsd.names'), 'w')
+	file.write('\n'.join(labels_dict.keys()))
+	file.close()
+
+if __name__=='__main__':
+	create_namefile()
