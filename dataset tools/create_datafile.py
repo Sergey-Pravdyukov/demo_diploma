96fca6568232dcf523c4e6beae3c555e18ea21f9
Sergey-Pravdyukov <sergey.pravdyukov@gmail.com>
2020-04-05 15:08:53 +0300

    Added datafile target that creates *.data file with respect to earlier create imagefiles and *.name file.

---
+++ dataset tools/create_datafile.py
@@ -0,0 +1,24 @@
+import click 
+import os
+from convert_annotated_RTSD_data_to_YOLO_format import get_annotation_lines, get_all_labels
+
+@click.command()
+@click.option('--dataset_path', default='../pruned_RTSD/detection/rtsd-d1-frames', help='Relative path to convertible dataset home dir.')
+@click.option('--dataset_annotations_path', default='../pruned_RTSD/detection/rtsd-d1-gt/rtsd-d1-gt_full.csv', help='Relative path to full datase toannotation files.')
+@click.option('--namefile_path', default='../config/rtsd.names', help='Path to dataset namefile.')
+@click.option('--target_dir', default='../config', help='Path to created *.data')
+def create_datafile(dataset_path, dataset_annotations_path, namefile_path, target_dir):
+	datafile_args = {}
+
+	datafile_args['classes'] = len(get_all_labels(get_annotation_lines(dataset_annotations_path)))
+	datafile_args['train'] = os.path.join(dataset_path, 'labels', 'train.txt')
+	datafile_args['valid'] = os.path.join(dataset_path, 'labels', 'val.txt')
+	datafile_args['names'] = namefile_path
+
+	file = open(os.path.join(target_dir, 'rtsd.data'), 'w')	
+	for key, value in datafile_args.items():
+		file.write("%s=%s\n" %(key, value))
+	file.close()
+
+if __name__=='__main__':
+	create_datafile()
